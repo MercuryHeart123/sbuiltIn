6a9d791948b9330a2fdcd88cea3af98cbb746eca
heart <heartkung35@gmail.com>
2022-02-01 00:06:27 +0700

    - feat: moveable 3d object

--- sbuiltfont/src/pages/threeD.js
+++ sbuiltfont/src/pages/threeD.js
@@ -1,26 +1,143 @@
-import React, {useState, useRef} from 'react'
-import { Canvas, extend, useThree, useFrame } from 'react-three-fiber'
+import React, {useState, useRef, useEffect, Suspense} from 'react'
+import { Canvas, useThree, useFrame } from 'react-three-fiber'
 import { useSpring, a } from '@react-spring/three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls' 
+import { useDrag } from "@use-gesture/react"
+import * as THREE from "three"
+import { OrbitControls, useHelper } from '@react-three/drei'
 import './style.css'
-extend({OrbitControls})
-    const Control= () => {
-        const orbitRef = useRef();
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
 
-        const { camera, gl } = useThree()
-        useFrame(() => {
-            orbitRef.current.update()
-        })
-        return(
-            <orbitControls
-                autoRotate
-                maxPolarAngle={Math.PI/3}
-                minPolarAngle={Math.PI/3}
-                args={[camera, gl.domElement]}
-                ref={orbitRef}
+const Control= (props) => {
+    const orbitRef = useRef(null);
+    const { camera, gl } = useThree()
+
+    useFrame(() => {
+        orbitRef.current.update()
+    })
+
+
+    return(
+        <>
+        <OrbitControls
+        
+            maxPolarAngle={Math.PI/3}
+            minPolarAngle={Math.PI/3}
+            args={[camera, gl.domElement]}
+            ref={orbitRef}
+            enabled={!props.type}
+        />
+
+        </>
+        
+    )
+}
+
+const BoxImport = ({ dimension, setIsDrag, plane }) => {
+    const dragObjectRef  = useRef();
+    
+    const [pos, setPos] = useState([0, 0, 0]);
+    const { size, viewport } = useThree();
+    const [ turn, setTurn] = useState(0)
+    const [model, setModel] = useState(null)
+    const [ modelSize, setModelSize] =  useState({x:0,y:0,z:0})
+    const aspect = size.width / viewport.width;
+    let planeIntersectPoint = new THREE.Vector3();
+    const [spring, api] = useSpring(() => ({}));
+    
+      const bind = useDrag(
+        ({ active, movement: [x, y], timeStamp, event }) => {
+          if (active) {
+            event.ray.intersectPlane(plane, planeIntersectPoint);
+            if(Math.abs(planeIntersectPoint.x) + modelSize.x  <= dimension.x/2 
+                && Math.abs(planeIntersectPoint.z) + modelSize.z <= dimension.z/2)
+            {
+                setPos([planeIntersectPoint.x, pos[1], planeIntersectPoint.z]);
+            }
+            
+          }
+          
+          setIsDrag(active);
+    
+          
+          return timeStamp;
+        },
+      );
+    
+    useEffect(async() => {
+        if(!model){
+            const loader = new GLTFLoader();
+            const dracoLoader = new DRACOLoader();
+            dracoLoader.setDecoderPath( 'three/examples/js/libs/draco/' );
+            loader.setDRACOLoader( dracoLoader );
+            loader.load('model/box.gltf', async function(gltf){
+                
+                gltf.scene.traverse( function ( child ) {
+                    if ( child.isMesh ) {
+                        child.material.color.set( 0xFF9021 );
+                        child.geometry.center(); // center here
+                    }
+                });
+                var box = new THREE.Box3().setFromObject( gltf.scene );
+                setModelSize(box.max)
+                if(box.min.y < 0){
+                    setPos([0,box.max.y,0])
+                }
+                setModel(gltf)
+            })
+        }
+        
+    })
+     
+    return ( model ?
+            
+            <a.mesh 
+            
+            onClick={(e) => {
+                if(e.altKey){
+                    setTurn(turn + Math.PI/2)
+                    setModelSize({
+                        x:modelSize.z,
+                        y:pos[1],
+                        z:modelSize.x
+                    })
+                }
+            }}
+            
+
+            {...spring} {...bind()} castShadow>
+                <primitive
+                position={pos}
+                rotation={[0,turn,0]}
+                
+                ref={dragObjectRef}
+                object={model.scene}
+                >
+
+                </primitive>
+            </a.mesh>
+            :
+            null
+        
+
+        
+    )
+}
+
+const Plane = ({dimension}) => {
+    const planeRef = useRef()
+
+    useHelper(planeRef, THREE.BoxHelper, 'red')
+    return (
+        <mesh ref={planeRef} position={[0, dimension.y/(-2), 0]} scale={[dimension.x,dimension.y,dimension.z]} userData={{ground:true}} >
+            <boxBufferGeometry
+                attach="geometry"
+
             />
-        )
-    }
+                <meshPhysicalMaterial attach="material" color="green" />
+        </mesh>
+    )
+}
 
 const Box = () =>{
     const meshRef = useRef();
@@ -45,26 +162,43 @@ const Box = () =>{
                 setActive(!active)
             }}
             scale={props.scale}
+            castShadow ={true}
+            receiveShadow={true}
     >
         <boxBufferGeometry
             attach="geometry"
 
         />
-        <ambientLight/>
-        <spotLight position={[0, 5, 10]}/>
-        <a.meshPhysicalMaterial
-            attach='material' 
-            color={props.color}/>
+        
+            <a.meshPhysicalMaterial
+                attach='material' 
+                color={props.color}/>
     </a.mesh>)
 }
     
 const ThreeD = () =>{
+        const [isDrag, setIsDrag] = useState(false)
+        const [ planeD, setPlaneD] = useState({x:5,y:1,z:5})
+        const floorPlane = new THREE.Plane(new THREE.Vector3(0, 1, 0), 0);
+        
         return(
-            <div style={{display:'block'}}>
+            <div>
+                <button onClick={()=>{setPlaneD(
+                    {x:planeD.x+1,
+                    y:1,
+                    z:planeD.z+1,}
+
+                )}}>change</button>
+            <Suspense fallback={null} style={{display:'block'}}>
                 <Canvas>
-                    <Control/>
-                    <Box/>
+                    <Control type={isDrag}/>
+                    <ambientLight/>
+                    <Plane dimension={planeD}/>
+                    <spotLight position={[0, 5, 10]} />
+                    <BoxImport dimension={planeD} setIsDrag={setIsDrag} plane={floorPlane}/>
+                    {/* <Box/> */}
                 </Canvas>
+            </Suspense>
             </div>
             
         )
