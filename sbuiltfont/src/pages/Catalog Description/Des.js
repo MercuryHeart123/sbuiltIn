34583d97ddbfadb11e9aa3d9ea1dda8fd9390f98
minstone <mumin96303@gmail.com>
2022-03-18 22:58:45 +0700

    -Update Detail catalog page responsive & backbtn

--- sbuiltfont/src/pages/Catalog Description/Des.js
+++ sbuiltfont/src/pages/Catalog Description/Des.js
@@ -1,21 +1,18 @@
 import React from "react";
 import SimpleImageSlider from "react-simple-image-slider";
 import StackGrid from "react-stack-grid";
-
+import { Column ,Title} from "./Des.element";
+import { IoChevronBack } from 'react-icons/io5';
+import { Link } from 'react-router-dom'
+import './style.css'
 const Desc = () => {
-
     const style = {
         row:{
             display:'flex',
             flexWrap:'wrap',
-            // height:'90vh',
+            // height:'100vh',
             padding:'10px 80px 8px 80px',
             fontFamily:'Prompt'
-        },
-        column:{
-            flex:'1',
-            maxWidth:'50%',
-            padding:'60px'
         },
         Imgstyle:{
             width:'100%', 
@@ -23,7 +20,6 @@ const Desc = () => {
             margin:'5px 0 5px 0',
           }
     }
-
     const images=[
         {
           key: '0',
@@ -59,16 +55,23 @@ const Desc = () => {
     
     return(
         <>
+        <Link to={'/catalog'}>
+            <div className="Backbtn">
+                <IoChevronBack />
+            </div>
+        </Link>
         <div className="Descpage" style={style.row}>
-            <div className="Image" style={style.column}>
-                <img src="https://cdn.pixabay.com/photo/2014/10/01/10/44/hedgehog-468228_960_720.jpg" style={{width:'100%', borderRadius:'18px'}}></img>
-            </div>
-            <div className="description" style={style.column}>
-            <h3>Title</h3>
-            <span style={{fontSize:'10px'}}>@18/3/2022</span>
-            <br />
-            <span style={{fontSize:'13px'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap</span>
-            </div>
+                <Column className="column">
+                    <img src="https://cdn.pixabay.com/photo/2014/10/01/10/44/hedgehog-468228_960_720.jpg" style={{width:'100%', borderRadius:'18px'}}></img>
+                </Column>
+                <Column>
+                <Title>
+                <h3>Title</h3>
+                <span style={{fontSize:'10px'}}>@18/3/2022</span>
+                </Title>
+                <br />
+                <span style={{fontSize:'13px'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap</span>
+                </Column>
         </div>
 
         <div style={{marginBottom:'27px'}}>
